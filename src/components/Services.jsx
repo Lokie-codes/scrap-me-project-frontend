5a17b407fdb32c8d8c438c4b5574db53017a3663
Lokesh <lokeshlucky13579@gmail.com>
2023-07-21 19:01:37 +0530

    removal of gradient background

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Services = ({ services }) => {
     return (
         <div className='services-container bg-gradient-to-b from-green-600 via-green-500 to-green-600 rounded-lg mx-7 my-5 py-5 px-4 bg-cover '>
-            <h1 className='text-center text-5xl sm:text-3xl py-3 text-slate-100 drop-shadow-xl md:text-4xl font-bold lg:text-5xl'>Our Services</h1>
+            <h1 className='text-center text-5xl sm:text-3xl py-3 text-black drop-shadow-xl md:text-4xl font-bold lg:text-5xl'>Our Services</h1>
             <div className='flex flex-row sm:flex-col md:flex-col lg:flex-row flex-wrap items-center justify-center'>
                 {services.map((service) => {
                     return (
