85d8ebb4746ae11cab4a4c247b7ac6c4f4b5331f
Lokesh <lokeshlucky13579@gmail.com>
2023-07-21 20:34:24 +0530

    addition of callback support image

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -3,6 +3,7 @@ import mapIcon from '../icons/map-icon.svg';
 import facebookIcon from '../icons/facebook-icon.svg';
 import instagramIcon from '../icons/instagram-icon.svg';
 import contactPhoto from '../images/contact-photo.png';
+import callBackImg from '../images/callback-photo.png';
 
 const ContactUs = ({ contact }) => {
     return (
@@ -30,33 +31,39 @@ const ContactUs = ({ contact }) => {
                         <img src={contactPhoto} alt="contact/png" />
                     </div>
                 </div>
-                <div className='grid items-center justify-center m-3 gap-3'>
-                    <h2 className='text-xl text-center bg-slate-200 drop-shadow-lg rounded-lg px-3 py-2'>or get a callback from us</h2>
-                    <form className='grid items-center justify-center bg-white rounded-lg px-3 py-4 gap-3'>
-                        {/* Name */}
-                        <div className='flex items-center justify-center gap-2 px-3 py-2 mx-1'>
-                            <label htmlFor="name" className='drop-shadow-md text-center m-1/2'>Name</label>
-                            <input type="text" placeholder='John Doe' autoComplete='name' className='shadow-lg m-1/2 px-5 py-3 rounded-md w-full bg-slate-300' id='name' name='name' />
-                        </div>
-                        {/* Phone */}
-                        <div className='flex items-center justify-center gap-2 px-3 py-2 mx-1'>
-                            <label htmlFor="phone" className='drop-shadow-md text-center m-1/2'>Phone</label>
-                            <input type="tel" name="phone" id="phone" autoComplete='tel' className='shadow-lg m-1/2 px-5 py-3 rounded-md w-full bg-slate-300' placeholder='9876543210' />
-                        </div>
-                        {/* Email */}
-                        <div className='flex items-center justify-center gap-2 px-3 py-2 mx-1'>
-                            <label htmlFor="email" className='drop-shadow-md text-center m-1/2'>Email</label>
-                            <input type="email" name="email" id="email" autoComplete='email' className='shadow-lg m-1/2 px-5 py-3 rounded-md w-full bg-slate-300' placeholder='john.doe@example.com' />
-                        </div>
-                        {/* Location */}
-                        <div className='flex items-center justify-center gap-2 px-3 py-2 mx-1'>
-                            <label htmlFor="location" className='drop-shadow-md text-center m-1/2 '>Location</label>
-                            <input type="text" name="location" id="location" className='shadow-lg m-1/2 px-5 py-3 rounded-md w-full bg-slate-300' autoComplete='address-level2' placeholder='cityname' />
-                        </div>
-                        <div className='flex items-center justify-center'>
-                            <button type="submit" className='rounded-xl drop-shadow-lg shadow-xl bg-green-500 m-2 p-3'>Submit</button>
-                        </div>
-                    </form>
+
+                <div className='flex items-center justify-center'>
+                    <div className='w-1/3 m-3 p-5 sm:hidden md:hidden lg:w-1/3'>
+                        <img src={callBackImg} alt="callback/png" />
+                    </div>
+                    <div className='w-1/2 sm:w-fit md:w-fit lg:w-fit grid items-center justify-center m-3 gap-3'>
+                        <h2 className='text-xl text-center bg-slate-200 drop-shadow-lg rounded-lg px-3 py-2'>or get a callback from us</h2>
+                        <form className='grid items-center justify-center bg-white rounded-lg px-3 py-4 gap-3'>
+                            {/* Name */}
+                            <div className='flex items-center justify-center gap-2 px-3 py-2 mx-1'>
+                                <label htmlFor="name" className='drop-shadow-md text-center m-1/2'>Name</label>
+                                <input type="text" placeholder='John Doe' autoComplete='name' className='shadow-lg m-1/2 px-5 py-3 rounded-md w-full bg-slate-300' id='name' name='name' />
+                            </div>
+                            {/* Phone */}
+                            <div className='flex items-center justify-center gap-2 px-3 py-2 mx-1'>
+                                <label htmlFor="phone" className='drop-shadow-md text-center m-1/2'>Phone</label>
+                                <input type="tel" name="phone" id="phone" autoComplete='tel' className='shadow-lg m-1/2 px-5 py-3 rounded-md w-full bg-slate-300' placeholder='9876543210' />
+                            </div>
+                            {/* Email */}
+                            <div className='flex items-center justify-center gap-2 px-3 py-2 mx-1'>
+                                <label htmlFor="email" className='drop-shadow-md text-center m-1/2'>Email</label>
+                                <input type="email" name="email" id="email" autoComplete='email' className='shadow-lg m-1/2 px-5 py-3 rounded-md w-full bg-slate-300' placeholder='john.doe@example.com' />
+                            </div>
+                            {/* Location */}
+                            <div className='flex items-center justify-center gap-2 px-3 py-2 mx-1'>
+                                <label htmlFor="location" className='drop-shadow-md text-center m-1/2 '>Location</label>
+                                <input type="text" name="location" id="location" className='shadow-lg m-1/2 px-5 py-3 rounded-md w-full bg-slate-300' autoComplete='address-level2' placeholder='cityname' />
+                            </div>
+                            <div className='flex items-center justify-center'>
+                                <button type="submit" className='rounded-xl drop-shadow-lg shadow-xl bg-green-500 m-2 p-3'>Submit</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div >
